aa07910323641594e6408fb5a269c8924e4d80d3
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2023-05-23 12:32:07 +1000

    Initial Commit

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+describe('Unit Test', () => {
+  it("TC001 Unit Test", () => {
+    expect(true).toBe(true);
+  });
+});
